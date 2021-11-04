f5d7d75fef75eebc537282ff4d478c5e7d780f2b
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-11-04 15:29:44 +0100

    video minute 19

---
+++ README.md
@@ -0,0 +1,5 @@
+## OOP
+
+This is the video in which this repo is based on
+
+https://www.youtube.com/watch?v=Ej_02ICOIgs
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 class Item:
+
+	def __init__(self, name):
+		print(f"An instance created: {name}")
+		self.name = name
+
 	def calculate_total_price(self, quantity, price):
 		return quantity * price
 
-item1 = Item()
-item2 = Item()
+item1 = Item('Phone')
+item2 = Item('Laptop')
 
-item1.name = 'Phone'
 item1.price = 100
 item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
 
-item2.name = 'Laptop'
 item2.price = 1000
 item2.quantity = 3
-print(item2.calculate_total_price(item2.price, item2.quantity))
+
+print(item1.name)
+print(item2.name)
