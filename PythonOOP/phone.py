3b0c581d456d86f525f58c29d2b42a7f0c7d355d
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-11-04 20:41:24 +0100

    1h 33 min

---
+++ PythonOOP/phone.py
@@ -0,0 +1,14 @@
+from item import Item
+
+class Phone(Item):
+	def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+		# Call to super function to have access to all attributes / methods
+		super().__init__(
+			name, price, quantity
+		)
+
+		#  run validation for received parameters
+		assert broken_phones >= 0, f"Broken phones {broken_phones} is not greater or equal than zero!"
+
+		# assign to self object
+		self.broken_phones = broken_phones
