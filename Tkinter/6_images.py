dd3f5caec8ceca3efe0817c6be9200daa5604a22
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-11-10 22:33:24 +0100

    update file

--- Tkinter/6_images.py
+++ Tkinter/6_images.py
@@ -5,7 +5,7 @@ root = Tk()
 root.title("Learn to Code at Codemy.com")
 root.iconbitmap('/Users/xabier/Documents/FreeCodeCamp/Tkinter/icons/trash_bin_recycle_bin_waste_dumpster_icon_191911.ico')
 
-my_image = ImageTk.PhotoImage(Image.open('images/dices.png'))
+my_image = ImageTk.PhotoImage(Image.open('/Users/xabier/Documents/FreeCodeCamp/Tkinter/images/dices.png'))
 my_label = Label(image=my_image)
 my_label.pack()
 
