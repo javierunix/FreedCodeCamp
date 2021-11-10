eaebaa2e972fee4d3bde5fe89469d8c8265d7323
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-11-10 16:18:31 +0100

    insert images

---
+++ Tkinter/6_images.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from PIL import ImageTk, Image # for managing usual images files (jpg, png,...)
+
+root = Tk()
+root.title("Learn to Code at Codemy.com")
+root.iconbitmap('/Users/xabier/Documents/FreeCodeCamp/Tkinter/icons/trash_bin_recycle_bin_waste_dumpster_icon_191911.ico')
+
+my_image = ImageTk.PhotoImage(Image.open('images/dices.png'))
+my_label = Label(image=my_image)
+my_label.pack()
+
+
+button_quit = Button(root, text="Exit Program", command=root.quit)
+button_quit.pack()
+
+
+root.mainloop()
