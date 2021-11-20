18373c79740ff6c9b85f4a6371768ccc242dd363
javierunix <fjguijarromartinez@gmail.com>
2021-11-20 10:52:23 +0100

    finish change color

--- Tkinter/18_weather.py
+++ Tkinter/18_weather.py
@@ -4,10 +4,8 @@ import json
 
 root = Tk()
 root.title('Codemy.com - Learn to Code')
-root.geometry("400x32")
+root.geometry("600x32")
 
-
-# create a requests variables
 
 # dictionary to associate quality with color
 category_color = {
@@ -20,14 +18,19 @@ category_color = {
     "Unavailable": "#E1EBF4"
 }
 
+# create a requests variables
 
-api_url = "https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=89104&date=2021-11-18&distance=5&API_KEY=D3C5E5E5-4893-4669-8819-58EBFFF1797D"
+api_url = "https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=93280&date=2021-11-20&distance=25&API_KEY=D3C5E5E5-4893-4669-8819-58EBFFF1797D"
+
+
 try:
     api_requests = requests.get(api_url)
     api = json.loads(api_requests.content)
     city = api[0]['ReportingArea']
     quality = str(api[0]['AQI'])
     category = api[0]['Category']['Name']
+    root.configure(background=category_color[category])
+
     my_label = Label(root, text=city + ": Quality " + quality
                      + " (" + category + ")", font=("Helvetica", 20),
                      background=category_color[category])
